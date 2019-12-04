37788fe3dc8dc6db2dc3edb606914474dd37780a
Morgan Rose <morganrose@dhcp-10-250-242-199.harvard.edu>
2019-12-03 20:39:38 -0500

    Commented

--- cm_analysis/data.R
+++ cm_analysis/data.R
@@ -2,25 +2,48 @@
 # Load in necessary libraries
 
 library(fs)
-library(tm)
 library(rvest)
 library(janitor)
-library(memoise)
 library(tidytext)
-library(wordcloud)
 library(tidyverse)
-library(RColorBrewer)
 
-# Load in script data from online (will comment specifics later)
+# Load in script data from online
+# Load in base url for scrapping
 
 base_url <- "https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=criminal-minds&episode="
-s <- c(rep(1, each = 22), rep(2, each = 22), rep(3, each = 20), rep(4, each = 26), rep(5, each = 23))
+
+# Specify the season for the url by noting how many scripts we'll need for each
+# season. For example, there are 22 scripts to scrap for season 1 and so on.
+
+s <- c(rep(1, each = 22), 
+       rep(2, each = 22),
+       rep(3, each = 20), 
+       rep(4, each = 26), 
+       rep(5, each = 23))
+
+# Add the season code to url
+
 season <- paste0("s0", s)
-ep <- c(rep(1:22, each = 1), rep(1:20, each = 1), rep(22:23, each = 1), rep(1:20, each = 1), rep(1:26, each = 1), rep(1:23, each = 1))
+
+# Specify the specific episodes to add to the url. Note, that the script for
+# season 2 episode 21 is not on the website, so manually skipped that.
+
+ep <- c(rep(1:22, each = 1), 
+        rep(1:20, each = 1), 
+        rep(22:23, each = 1), 
+        rep(1:20, each = 1), 
+        rep(1:26, each = 1), 
+        rep(1:23, each = 1))
+
+# Add episode code to url
+
 episode <- ifelse(ep < 10, paste0("e0", ep), paste0("e", ep))
+
+# Create place to store scripts
+
 all.scripts <- NULL
 
-#s02e21 missing from website, so had to adjust
+# Scrape scripts by pasting together the season/episode elements from above
 
 for (i in 1:113) {
   url <- paste0(base_url, season[i], episode[i])
@@ -29,7 +52,8 @@ for (i in 1:113) {
   all.scripts[i] <- html_text(script, trim = TRUE)
 }
 
-# put all scripts into data frame, make a tibble
+# Create a tibble to store all the scripts adding the season and episode code to
+# the rows
 
 cm <- as.data.frame(all.scripts, stringsAsFactors = FALSE)
 counter <- paste0(season, episode)
@@ -37,17 +61,15 @@ row.names(cm) <- counter[1:113]
 colnames(cm) <- "text"
 cm_scripts <- as_tibble(cm)
 
-# add episode column
+# Add episode column to script tibble
 
 cm_scripts <- cm_scripts %>% 
   mutate(episode = paste0(season, episode)) %>% 
   select(episode, text)
 
-
-# Read in csv data and specify column types, filter out the missing episode
-# Get rid of 2x21 since that script is missing
-# Change episode column format to match script tibble
-# Put episode column first
+# Read in csv data and specify column types, filter out the missing episode. Get
+# rid of 2x21 since that script is missing. Change episode column format to match
+# script tibble. Put episode column first just so it's easier to understand data
 
 cm_data <- read_csv("./cm_analysis/cm_data.csv", col_type = cols(
   ep = col_character(),
@@ -74,13 +96,15 @@ cm_data <- read_csv("./cm_analysis/cm_data.csv", col_type = cols(
 
 cm_joint <- left_join(cm_scripts, cm_data, by = "episode")
 
-# Create lists of important buzzwords
+# Create list of main character names
 
 bau <- c("jason", "gideon", "aaron", "hotch", "hotchner", 
          "david", "dave", "rossi", "spencer", "spence", 
          "reid", "derek", "morgan", "jennifer", "jj", 
          "jareau", "emily", "prentiss", "penelope", 
          "garcia", "elle", "greenaway")
+
+# Create list of important buzzwords
 
 buzzwords <- c("unsub", "suspect", "murder", "criminal", 
                "blood", "kill", "killer", "serial", "killers", 
@@ -92,8 +116,8 @@ buzzwords <- c("unsub", "suspect", "murder", "criminal",
                "thrill", "con", "proxy", "gangster", "copycat", 
                "terrorist", "cannibal", "criminals")
 
-# Separate the words, only include pre-selected buzzwords
-# Add season column
+# Separate each script into individual words, only include pre-selected
+# buzzwords. These buzzwords are based off my knowledge of the show.
 
 cm_words <- cm_joint %>% 
   unnest_tokens(word, text) %>% 
@@ -107,6 +131,8 @@ cm_words <- cm_joint %>%
            )) %>%
   filter(word %in% c(buzzwords, bau))
 
+# Add season column
+
 cm_season <- cm_data %>% 
   mutate(season =
            case_when(
@@ -119,8 +145,8 @@ cm_season <- cm_data %>%
 
 # Write object out to use in shiny app later
 
-write_rds(cm_words, "./cm_analysis/cm_words.rds")
+write_rds(cm_words, "./cm_analysis/objects/cm_words.rds")
 
-write_rds(cm_season, "./cm_analysis/cm_season.rds")
+write_rds(cm_season, "./cm_analysis/objects/cm_season.rds")
 
 

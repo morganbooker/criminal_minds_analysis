b5fe4f632c57afad5cd0a79bb3269d0de9aa2584
Morgan Rose <morganrose@dhcp-10-250-242-199.harvard.edu>
2019-12-03 20:40:47 -0500

    More plots added

--- cm_analysis/app.R
+++ cm_analysis/app.R
@@ -1,3 +1,6 @@
+
+# Load in necessary libraries
+
 library(shiny)
 library(knitr)
 library(fs)
@@ -9,44 +12,61 @@ library(tidytext)
 library(markdown)
 library(wordcloud)
 library(tidyverse)
+library(shinythemes)
 library(RColorBrewer)
+library(shinyWidgets)
 
-rmdfiles <- c("./about.Rmd")
-sapply(rmdfiles, knit, quiet = T)
 
-# Read in data from script
+# Read in data from scripts
 
-cm_words <- read_rds("./cm_words.rds")
+cm_words <- read_rds("./objects/cm_words.rds")
 
-cm_name <- read_rds("./cm_name.rds")
+cm_name <- read_rds("./objects/cm_name.rds")
 
-cm_name_season <- read_rds("./cm_name_season.rds")
+cm_name_season <- read_rds("./objects/cm_name_season.rds")
 
-cm_buzz <- read_rds("./cm_buzz.rds")
+cm_buzz <- read_rds("./objects/cm_buzz.rds")
 
-cm_buzz_season <- read_rds("./cm_buzz_season.rds")
+cm_buzz_season <- read_rds("./objects/cm_buzz_season.rds")
 
-cm_caught <- read_rds("./cm_caught.rds")
+cm_caught <- read_rds("./objects/cm_caught.rds")
 
-cm_caught_season <- read_rds("./cm_caught_season.rds")
+cm_caught_season <- read_rds("./objects/cm_caught_season.rds")
 
-# Create column for choices bar
+cm_gender <- read_rds("./objects/cm_gender.rds")
+
+cm_gender_season <- read_rds("./objects/cm_gender_season.rds")
+
+cm_crim <- read_rds("./objects/cm_crim.rds")
+
+cm_crim_season <- read_rds("./objects/cm_crim_season.rds")
+
+# Create word cloud (based on shiny website tutorial) and create column for word
+# cloud choices bar
 
 select_season <<- unique(cm_words$season)
 
 getTermMatrix <- memoise(function(season) {
   
+  # Specify that wordcloud should facet by season
+  
   if (!(season %in% select_season))
     stop("Unknown season")
   
+  # Specify that words should appear based on their frequency per season
+  
   words <- cm_words$word[cm_words$season == season]
+  
+  # Focus on specified words and remove any punctuation, numbers, and basic
+  # words like "that, "and", etc.
   
   myCorpus = Corpus(VectorSource(words))
   myCorpus = tm_map(myCorpus, content_transformer(tolower))
   myCorpus = tm_map(myCorpus, removePunctuation)
   myCorpus = tm_map(myCorpus, removeNumbers)
   myCorpus = tm_map(myCorpus, removeWords,
-                    c(stopwords("SMART"), "that", "he", "she", "the", "and", "but"))
+                    c(stopwords("SMART"), "that", 
+                      "he", "she", "the", "and", "but"))
   
   myDTM = TermDocumentMatrix(myCorpus,
                              control = list(minWordLength = 1))
@@ -57,39 +77,63 @@ getTermMatrix <- memoise(function(season) {
   
 })
 
-# general overview of data options
+
+# Create options for input selector for General Overview tab
 
 general_options <- c("Character Names",
                      "Buzzwords",
-                     "Criminal Caught?")
+                     "Criminal Caught?",
+                     "Criminal Gender",
+                     "Criminal Types")
+
+general_options_season <- c("Character Names",
+                     "Buzzwords",
+                     "Criminal Caught?",
+                     "Criminal Gender",
+                     "Criminal Types")
 
 ui <- navbarPage(
     "Criminal Minds Analysis",
+    
+    theme = shinytheme("cyborg"),
+    
     tabPanel("General Overview",
-             fluidPage(
-               titlePanel("Overview of Data"),
-               sidebarLayout(
-                 sidebarPanel(
-                   selectInput("general_op",
-                               "Area of Focus",
-                               choices = general_options,
-                               selected = general_options[1])
-                 ),
-                 mainPanel(
-                   plotOutput("cm_name"),
-                   plotOutput("cm_name_season"),
-                   plotOutput("cm_buzz"),
-                   plotOutput("cm_buzz_season"),
-                   plotOutput("cm_caught"),
-                   plotOutput("cm_caught_season")
-                 )
-               )
-             )),
-    tabPanel("Catching Criminals",
-             fluidPage(
-               titlePanel("Caught by Season"),
-               p("Work in progress")
-             )),
+             tabsetPanel(
+               tabPanel("All Seasons",
+                        sidebarLayout(
+                          sidebarPanel(
+                            
+                            # Create input selector
+                            
+                            selectInput("general_op",
+                                        "Area of Focus",
+                                        choices = general_options,
+                                        selected = general_options[1])
+                          ),
+                          
+                          mainPanel(
+                            plotOutput("cm_overview")
+                          )
+                        )),
+               tabPanel("By Season",
+                        sidebarLayout(
+                          sidebarPanel(
+                            
+                            # Create input selector
+                            
+                            selectInput("general_op_season",
+                                        "Area of Focus",
+                                        choices = general_options_season,
+                                        selected = general_options_season[1])
+                          ),
+                          
+                          mainPanel(
+                            plotOutput("cm_overview_season")
+                          )
+                        ))
+             ),
+             ),
+    
     tabPanel("Word Cloud",
              fluidPage(
                titlePanel("Word Cloud"),
@@ -113,53 +157,65 @@ ui <- navbarPage(
                  )
                )
              )),
-    tabPanel("Modeling",
+    
+    
+    tabPanel("Criminal Analysis",
              fluidPage(
-               p("Modelesque discussion to be added later as per Preceptor piazza note"),
+               titlePanel("Caught by Season"),
+               p("Work in progress")
+             )),
+    
+    tabPanel("Future Models Discussion",
+             fluidPage(
+               p("A way to further this project would be to build a 
+                  model that predicts if the criminal will be caught 
+                  based on the frequency of certain words. Building 
+                  that sort of model is beyond the scope of this course."),
              )),
     tabPanel("About",
              fluidPage(
-                 withMathJax(includeMarkdown("about.md"))
+               withMathJax(includeMarkdown("about.Rmd"))
              ))
 )
 
 server <- function(input, output) {
   
-  output$cm_name <- renderPlot({
+  output$cm_overview <- renderPlot({
     if(input$general_op == "Character Names") {
       cm_name
     }
-  })
-  
-  output$cm_name_season <- renderPlot({
-    if(input$general_op == "Character Names") {
-      cm_name_season
-    }
-  })
-  
-  output$cm_buzz <- renderPlot({
-    if(input$general_op == "Buzzwords") {
+    else if(input$general_op == "Buzzwords") {
       cm_buzz
     }
-  })
-  
-  output$cm_buzz_season <- renderPlot({
-    if(input$general_op == "Buzzwords") {
-      cm_buzz_season
-    }
-  })
-  
-  output$cm_caught <- renderPlot({
-    if(input$general_op == "Criminal Caught?") {
+    else if(input$general_op == "Criminal Caught?") {
       cm_caught
     }
-  })
-  
-  output$cm_caught_season <- renderPlot({
-    if(input$general_op == "Criminal Caught?") {
-      cm_caught_season
+    else if(input$general_op == "Criminal Gender") {
+      cm_gender
+    }
+    else if(input$general_op == "Criminal Types") {
+      cm_crim
     }
   })
+  
+  output$cm_overview_season <- renderPlot({
+    if(input$general_op_season == "Character Names") {
+      cm_name_season
+    }
+    else if(input$general_op_season == "Buzzwords") {
+      cm_buzz_season
+    }
+    else if(input$general_op_season == "Criminal Caught?") {
+      cm_caught_season
+    }
+    else if(input$general_op_season == "Criminal Gender") {
+      cm_gender_season
+    }
+    else if(input$general_op_season == "Criminal Types") {
+      cm_crim_season
+    }
+  })
+
   
   terms <- reactive({
     input$update
@@ -178,16 +234,16 @@ server <- function(input, output) {
     v <- terms()
     wordcloud_rep(names(v), v, scale=c(4,0.5),
                   min.freq = input$freq, max.words=input$max,
-                  colors = brewer.pal(8, "RdPu"))
+                  colors = brewer.pal(8, "Spectral"))
   })
-  
+   
 }
+
+
 
 shinyApp(ui, server)
 
 # to do:
 # comment all files
-# figure out why word cloud is not showing words by season
 # maybe make bar plots interactive--hover over and you'll see the count
-# figure out why plots move down page in general overview section
 # create folder just for rds files since there are a lot

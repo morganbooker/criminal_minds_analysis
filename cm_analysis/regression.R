de2e9d61ded756eb57e7c60739b526430e6fb0c3
Morgan Rose <morganrose@dhcp-10-250-242-199.harvard.edu>
2019-12-12 20:36:25 -0500

    Added regressions

---
+++ cm_analysis/regression.R
@@ -0,0 +1,225 @@
+
+# Load in necessary libraries
+
+library(tidyverse)
+library(gt)
+library(moderndive)
+
+# Read in objects
+
+read_rds("./cm_analysis/objects/cm_season.rds")
+
+read_rds("./cm_analysis/objects/cm_words.rds")
+
+# Mutate the words so that all versions of a character's name (first name, last
+# name, nickname) all count as one word so it's easier to count the aggregate of
+# character name appearance instead of counting each separate part of the name
+# individually
+
+cm_words_bau <- cm_words %>% 
+  filter(word %in% bau) %>% 
+  mutate(word = case_when(
+    word == "spencer" | word == "spence" | word == "reid" ~ "Spencer Reid",
+    word == "derek" | word == "morgan" ~ "Derek Morgan",
+    word == "aaron" | word == "hotch" | word == "hotchner" ~ "Aaron Hotchner",
+    word == "david" | word == "dave" | word == "rossi" ~ "David Rossi",
+    word == "jason" | word == "gideon" ~ "Jason Gideon",
+    word == "emily" | word == "prentiss" ~ "Emily Prentiss",
+    word == "jennifer" | word == "jj" | word == "jareau" ~ "Jennifer Jareau",
+    word == "penelope" | word == "garcia" ~ "Penelope Garcia",
+    word == "elle" | word == "greenaway" ~ "Elle Greenaway"
+  ))
+
+# Filter words to only look at the buzzwords
+
+cm_words_buzz <- cm_words %>% 
+  filter(word %in% buzzwords)
+
+# Modify caught so it is a numeric binary dummy variable and drop NAs. Group
+# by season for analysis.
+
+mod_data <- cm_words_bau %>%
+  mutate(caught = case_when(
+    caught == "yes" ~ "1",
+    caught == "no" ~ "0"
+  ),
+  caught = as.numeric(caught)) %>% 
+  drop_na(caught) %>% 
+  group_by(season)
+
+mod_data_buzz <- cm_words_buzz %>%
+  mutate(caught = case_when(
+    caught == "yes" ~ "1",
+    caught == "no" ~ "0"
+  ),
+  caught = as.numeric(caught)) %>% 
+  drop_na(caught) %>% 
+  group_by(season)
+
+# Fit a regression model of character names on caught
+
+mod <- lm(caught ~ word, data = mod_data)
+
+# Create a gt table of regression points
+
+mod_table <- mod %>% 
+  
+  # Use tidy to get regression points
+  
+  tidy() %>% 
+  
+  # Rename the terms column to character names
+  
+  mutate(term = fct_recode(
+    term,
+    "David Rossi" = "wordDavid Rossi",
+    "Derek Morgan" = "wordDerek Morgan",
+    "Elle Greenaway" = "wordElle Greenaway",
+    "Emily Prentiss" = "wordEmily Prentiss",
+    "Jason Gideon" = "wordJason Gideon",
+    "Jennifer Jareau" = "wordJennifer Jareau",
+    "Penelope Garcia" = "wordPenelope Garcia",
+    "Spencer Reid" = "wordSpencer Reid"
+  )) %>% 
+  
+  gt() %>% 
+  
+  # Hide the test statstic column because won't use it for analysis
+  
+  cols_hide("statistic") %>% 
+  
+  # Rename the column labels to make them look prettier 
+  
+  cols_label(
+    term = "Character Name",
+    estimate = "Estimate",
+    std.error = "Standard Error",
+    p.value = "P-Value"
+  ) %>% 
+  
+  # Realign the columns so they look nicer
+  
+  cols_align(
+    align = "left",
+    columns = vars(term)
+  ) %>% 
+  
+  cols_align(
+    align = "center",
+    columns = vars(estimate, std.error, p.value)
+  ) %>% 
+  
+  tab_header(title = "Examining the Relationship between\nCharacter Name 
+                      Frequency and Criminal Capture",
+             subtitle = "Based on the first five seasons of Criminal Minds") %>% 
+  
+  # Round all the values to 3 decimal points
+  
+  fmt_number(columns = vars(estimate, std.error, p.value), decimals = 3) %>% 
+  
+  # Change the color of the table to make it look better with shiny theme
+  
+  tab_options(table.background.color = "lightskyblue")
+
+
+# Fit a regression model of character names on caught
+
+mod_buzz <- lm(caught ~ word, data = mod_data_buzz)
+
+# Create a gt table of regression points
+
+mod_table_buzz <- mod_buzz %>% 
+  
+  # Use tidy to get regression points
+  
+  tidy() %>% 
+  
+  # Rename the terms column to buzzwords
+  
+  mutate(term = fct_recode(
+    term,
+    "Arson" = "wordarson",
+    "Arsonist" = "wordarsonist",
+    "Blood" = "wordblood",
+    "Bomber" = "wordbomber",
+    "Cannibal" = "wordcannibal",
+    "Con" = "wordcon",
+    "Copycat" = "wordcopycat",
+    "Criminal" = "wordcriminal",
+    "Criminals" = "wordcriminals",
+    "Hitman" = "wordhitman",
+    "Homicidal" = "wordhomicidal",
+    "Homicide" = "wordhomicide",
+    "Kidnapper" = "wordkidnapper",
+    "Kill" = "wordkill",
+    "Killer" = "wordkiller",
+    "Killers" = "wordkillers",
+    "Murder" = "wordmurder",
+    "Pedophile" = "wordpedophile",
+    "Poison" = "wordpoison",
+    "Profile" = "wordprofile",
+    "Proxy" = "wordproxy",
+    "Psychopath" = "wordpsychopath",
+    "Rape" = "wordrape",
+    "Rapist" = "wordrapist",
+    "Sadism" = "wordsadism",
+    "Sadist" = "wordsadist",
+    "Serial" = "wordserial",
+    "Signature" = "wordsignature",
+    "Sociopath" = "wordsociopath",
+    "Souvenir" = "wordsouvenir",
+    "Spree" = "wordspree",
+    "Stalker" = "wordstalker",
+    "Suspect" = "wordsuspect",
+    "Terrorist" = "wordterrorist",
+    "Thrill" = "wordthrill",
+    "Trophy" = "wordtrophy",
+    "Unsub" = "wordunsub",
+    "Victim" = "wordvictim",
+  )) %>% 
+  
+  gt() %>% 
+  
+  # Hide the test statstic column because won't use it for analysis
+  
+  cols_hide("statistic") %>% 
+  
+  # Rename the column labels to make them look prettier 
+  
+  cols_label(
+    term = "Buzzword",
+    estimate = "Estimate",
+    std.error = "Standard Error",
+    p.value = "P-Value"
+  ) %>% 
+  
+  # Realign the columns so they look nicer
+  
+  cols_align(
+    align = "left",
+    columns = vars(term)
+  ) %>% 
+  
+  cols_align(
+    align = "center",
+    columns = vars(estimate, std.error, p.value)
+  ) %>% 
+  
+  tab_header(title = "Examining the Relationship between Buzzword 
+                      Frequency and Criminal Capture",
+             subtitle = "Based on the first five seasons of Criminal Minds") %>% 
+  
+  # Round all the values to 3 decimal points
+  
+  fmt_number(columns = vars(estimate, std.error, p.value), decimals = 3) %>% 
+  
+  # Change the color of the table to make it look better with shiny theme
+  
+  tab_options(table.background.color = "lightskyblue")
+
+
+# Write out objects 
+
+write_rds(mod_table, "./cm_analysis/objects/mod_table.rds")
+
+write_rds(mod_table_buzz, "./cm_analysis/objects/mod_table_buzz.rds")
